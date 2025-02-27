d9058ef3a5202903791dba4c7c4dc97472531910
K4TR1NE <proono83@gmail.com>
2025-02-27 15:30:14 +0400

    Update import telebot.py

--- import telebot.py
+++ import telebot.py
@@ -1,7 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CramerMethod3x3
+namespace Задача_Задачи
 {
     public partial class Form1 : Form
     {
@@ -10,72 +17,58 @@ namespace CramerMethod3x3
             InitializeComponent();
         }
 
-        private void buttonSolve_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                // Получаем коэффициенты из текстовых полей
-                double a1 = double.Parse(textBoxA1.Text);
-                double b1 = double.Parse(textBoxB1.Text);
-                double c1 = double.Parse(textBoxC1.Text);
-                double d1 = double.Parse(textBoxD1.Text);
+                // Объявляем переменные
+                double a1 = double.Parse(textBox1.Text);
+                double a2 = double.Parse(textBox2.Text);
+                double a3 = double.Parse(textBox3.Text);
 
-                double a2 = double.Parse(textBoxA2.Text);
-                double b2 = double.Parse(textBoxB2.Text);
-                double c2 = double.Parse(textBoxC2.Text);
-                double d2 = double.Parse(textBoxD2.Text);
+                double b1 = double.Parse(textBox4.Text);
+                double b2 = double.Parse(textBox5.Text);
+                double b3 = double.Parse(textBox6.Text);
 
-                double a3 = double.Parse(textBoxA3.Text);
-                double b3 = double.Parse(textBoxB3.Text);
-                double c3 = double.Parse(textBoxC3.Text);
-                double d3 = double.Parse(textBoxD3.Text);
+                double c1 = double.Parse(textBox7.Text);
+                double c2 = double.Parse(textBox8.Text);
+                double c3 = double.Parse(textBox9.Text);
 
-                // Вычисляем главный определитель (Delta)
-                double delta = a1 * (b2 * c3 - b3 * c2)
-                            - b1 * (a2 * c3 - a3 * c2)
-                            + c1 * (a2 * b3 - a3 * b2);
+                double s1 = double.Parse(textBox10.Text);
+                double s2 = double.Parse(textBox11.Text);
+                double s3 = double.Parse(textBox12.Text);
 
-                // Вычисляем определитель для x (DeltaX)
-                double deltaX = d1 * (b2 * c3 - b3 * c2)
-                               - b1 * (d2 * c3 - d3 * c2)
-                               + c1 * (d2 * b3 - d3 * b2);
+                // Главный определитель
+                double D = a1 * b2 * c3 + a3 * b1 * c2 + a2 * b3 * c1 - a2 * b1 * c3 - a1 * b3 * c2 - a3 * b2 * c1;
 
-                // Вычисляем определитель для y (DeltaY)
-                double deltaY = a1 * (d2 * c3 - d3 * c2)
-                               - d1 * (a2 * c3 - a3 * c2)
-                               + c1 * (a2 * d3 - a3 * d2);
-
-                // Вычисляем определитель для z (DeltaZ)
-                double deltaZ = a1 * (b2 * d3 - b3 * d2)
-                               - b1 * (a2 * d3 - a3 * d2)
-                               + d1 * (a2 * b3 - a3 * b2);
-
-                // Проверяем, что система имеет решение
-                if (delta == 0)
+                // Проверка, имеет ли система решения
+                if (D != 0)
                 {
-                    if (deltaX == 0 && deltaY == 0 && deltaZ == 0)
-                    {
-                        textBoxResult.Text = "Система имеет бесконечное количество решений";
-                    }
-                    else
-                    {
-                        textBoxResult.Text = "Система не имеет решений";
-                    }
+                    // Дополнительные определители
+                    double D1 = s1 * b2 * c3 + s3 * b1 * c2 + s2 * b3 * c1 - s3 * b2 * c1 - s1 * b3 * c2 - s2 * b1 * c3;
+                    double D2 = a1 * s2 * c3 + a3 * s1 * c2 + a2 * s3 * c1 - a3 * s2 * c1 - a1 * s3 * c2 - a2 * s1 * c3;
+                    double D3 = a1 * b2 * s3 + a3 * b1 * s2 + a2 * b3 * s1 - a3 * b2 * s1 - a1 * b3 * s2 - a2 * b1 * s3;
+
+                    // Решения
+                    double x1 = D1 / D;
+                    double x2 = D2 / D;
+                    double x3 = D3 / D;
+
+                    // Вывод результата
+                    textBox13.Text = $"x1 = {x1}{Environment.NewLine}x2 = {x2}{Environment.NewLine}x3 = {x3}";
                 }
                 else
                 {
-                    // Находим решение системы
-                    double x = deltaX / delta;
-                    double y = deltaY / delta;
-                    double z = deltaZ / delta;
-
-                    // Выводим результат
-                    textBoxResult.Text = $"x = {x}, y = {y}, z = {z}";
+                    textBox13.Text = "Система имеет бесконечно много решений или не имеет решений.";
                 }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Ошибка: " + ex.Message);
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
